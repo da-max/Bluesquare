9135766ac1ebd152e60682079a4febb0fa48cc31
Maxime Ben Hassen <maxime.benhassen@fedabian.fr>
2023-05-02 10:39:35 +0200

    🔧 chore(conf): add configuration for vite and install vue

---
+++ docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+php artisan serve --host=0.0.0.0 -n &
+
+npm run dev
---
+++ resources/js/app.ts
@@ -0,0 +1,6 @@
+import './bootstrap'
+import '../css/app.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import { defineCustomElement } from 'vue'
