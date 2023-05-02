4c47b9b6433e459ae9b6767b87a8f7b6794d36e3
Maxime Ben Hassen <maxime.benhassen@fedabian.fr>
2023-05-02 14:28:15 +0200

    🔧 chore(conf): add configuration for vue and create first example customElement

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,3 +4,6 @@ import '../css/app.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 import { defineCustomElement } from 'vue'
+import Example from './Example.ce.vue'
+
+customElements.define('my-example', defineCustomElement(Example))
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('base')
 
 @section('content')
+    <my-example></my-example>
 @endsection
